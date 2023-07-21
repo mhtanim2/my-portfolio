229ad9e9d532a35fb678aa0172fd5621e43fadd7
mhtanim <mhtanim2@gmail.com>
2023-07-22 02:51:18 +0600

    added a clear image

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -11,24 +11,22 @@ const Footer = () => {
   return (
     <footer>
       <div>
-        <img
-          src={me}
-          alt="Founder"
-        />
-
+        <img src={me} alt="Founder" />
         <h2>Mehrab Hasan Tanim</h2>
         <p>Thanks for visiting.</p>
       </div>
-
       <aside>
         <h2>Social Media</h2>
 
         <article>
-        <a href="https://github.com/mhtanim2" target={"blank"}>
+          <a href="https://github.com/mhtanim2" target={"blank"}>
             <AiFillGithub />
           </a>
-          
-          <a href="https://www.linkedin.com/in/mehrab-hasan-tanim-b123301a6" target={"blank"}>
+
+          <a
+            href="https://www.linkedin.com/in/mehrab-hasan-tanim-b123301a6"
+            target={"blank"}
+          >
             <AiFillLinkedin />
           </a>
           <a href="https://leetcode.com/mhtanim2/" target={"blank"}>
